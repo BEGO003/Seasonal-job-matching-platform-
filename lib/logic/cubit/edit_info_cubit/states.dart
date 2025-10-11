3b314756b667ce5589b980e3e0bdcfc40fb15fed
Gumbile <garynik414@gmail.com>
2025-10-11 11:53:32 +0300

    still trying to work with git

--- lib/logic/cubit/edit_info_cubit/states.dart
+++
@@ -1,9 +0,0 @@
-abstract class EditInfoStates{}
-
-class EditInfoInitailState extends EditInfoStates{}
-
-class EditInfoLoadingState extends EditInfoStates{}
-
-class EditInfoSuccessState extends EditInfoStates{}
-
-class EditInfoErrorState extends EditInfoStates{}
