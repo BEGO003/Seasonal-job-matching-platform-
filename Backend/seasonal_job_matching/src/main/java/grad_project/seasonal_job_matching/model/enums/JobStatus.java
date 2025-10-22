ab408b9ed90792fcd00ab86b23f97eb25fb6bbeb
Bahoora <baheradawy@gmail.com>
2025-10-22 13:20:06 +0300

    SJMP-61<Adjusted JobStatus enum>

--- Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/model/enums/JobStatus.java
+++ Backend/seasonal_job_matching/src/main/java/grad_project/seasonal_job_matching/model/enums/JobStatus.java
@@ -1,7 +1,7 @@
 package grad_project.seasonal_job_matching.model.enums;
 
 public enum JobStatus {
-REVIEWING,
-INTERVIEW_SCHEDULED,
-HIRED
+OPEN, //change to OPEN
+CLOSED,
+DRAFT//add DRAFT, only available to employer/account
 }
