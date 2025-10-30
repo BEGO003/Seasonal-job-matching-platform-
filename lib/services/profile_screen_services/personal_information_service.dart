feabd99a6751622ab327ac711bf54d90fefb1297
Gumbile <garynik414@gmail.com>
2025-10-30 12:11:09 +0300

    commit for profile screen

--- lib/services/profile_screen_services/personal_information_service.dart
+++ lib/services/profile_screen_services/personal_information_service.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:job_seeker/core/dio_provider.dart';
 import 'package:job_seeker/endpoints.dart';
