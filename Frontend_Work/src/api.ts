06e9fe2142131dbce062e745e35abae3f232686c
BEGO003 <ahmedbahig2003@gmail.com>
2025-12-16 02:32:43 +0200

    Update api.ts

--- Frontend_Work/src/api.ts
+++ Frontend_Work/src/api.ts
@@ -126,10 +126,6 @@ const mapApiJobToJob = (j: any): Job => {
 const commonHeaders: HeadersInit = {
   Accept: "application/json",
   "Content-Type": "application/json",
-  "ngrok-skip-browser-warning": "true",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 // -------------------- Job API --------------------
